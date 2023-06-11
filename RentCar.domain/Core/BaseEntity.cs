6e0f184c59194b4ddd714e8bddd4d3767386ccf5
fcoscar <oscarjr01.13@gmail.com>
2023-06-11 12:05:46 -0400

    base entity FechaCreacion

--- RentCar.domain/Core/BaseEntity.cs
+++ RentCar.domain/Core/BaseEntity.cs
@@ -4,6 +4,10 @@ namespace RentCar.domain.Core
 {
     public class BaseEntity
     {
+        public BaseEntity()
+        {
+            FechaCreacion = DateTime.Now;
+        }
         public int? IdUsuarioCreacion { get; set; }
         public DateTime? FechaMod { get; set; }
         public DateTime FechaCreacion { get; set; }
