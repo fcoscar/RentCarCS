003d854fe0a10b14600015e79fe3a3cf870e8509
fcoscar <oscarjr01.13@gmail.com>
2023-06-12 20:30:50 -0400

    alquiler add

---
+++ RentCar.web/Models/Request/AlquilerAddResquest.cs
@@ -0,0 +1,13 @@
+namespace RentCar.web.Models.Request;
+
+public class AlquilerAddResquest
+{
+    public int Id { get; set; }
+    public int ReservationTime { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+    public int CarId { get; set; }
+    public DateTime Fecha { get; set; }
+    public int? IdUsuarioCreacion { get; set; }
+}
